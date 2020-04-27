e1e31cac23edb03156837a84e8656d1dac838cea
pjay79 <praveen.jayarajan@gmail.com>
2020-04-27 16:33:09 +1000

    refactor

--- Services/product.js
+++ Services/product.js
@@ -1,29 +1,29 @@
 const Product = require('../Model/product');
 
 module.exports = {
-async createProduct (product) {
-  const result = await Product.create(product);
-  if(result) {
-    return {
-      data: product,
-      message: "Product successfully created!"
-    };
-  }
-  return "Error creating new product"
-},
+  async createProduct (product) {
+    const result = await Product.create(product);
+    if(result) {
+      return {
+        data: product,
+        message: "Product successfully created!"
+      };
+    }
+    return "Error creating new product"
+  },
 
-async getAllProduct()  {
-  const product = await Product.find();
-  if(product) {
-    return product;
-  }
-  return "Error fetching products from db"
-},
-
-async getProductById(productId)  {
-  const product = await Product.findOne(productId);
-  if(product) {
+  async getAllProduct()  {
+    const product = await Product.find();
+    if(product) {
       return product;
-  }
-  return "Error fetching product from db";
+    }
+    return "Error fetching products from db"
+  },
+
+  async getProductById(productId)  {
+    const product = await Product.findOne(productId);
+    if(product) {
+        return product;
+    }
+    return "Error fetching product from db";
 }};
